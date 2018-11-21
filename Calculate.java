374daa1bd1bbff21bcf46e131fea544c1e11c819
walerra69 <t.valerrii@yandex.ru>
2018-11-21 09:31:32 +0300

    create Calculate1

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,11 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+
+                int first = Integer.valueOf(arg[0]);
+                int second = Integer.valueOf(arg[1]);
+                int summ = first + second;
+                System.out.println("Summ  "+ summ);
+                
 	}
 }
